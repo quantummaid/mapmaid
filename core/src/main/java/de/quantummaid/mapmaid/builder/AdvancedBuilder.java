c233437aba49eed4c0560c7cc2819e9bc89d35fd
QuantumMaid Developer <developer@quantummaid.de>
2020-03-12 10:06:06 +0100

    bump version

--- core/src/main/java/de/quantummaid/mapmaid/builder/AdvancedBuilder.java
+++ core/src/main/java/de/quantummaid/mapmaid/builder/AdvancedBuilder.java
@@ -54,9 +54,7 @@ import static java.util.stream.Collectors.toList;
 @RequiredArgsConstructor(access = AccessLevel.PRIVATE)
 public final class AdvancedBuilder {
     private static final List<Autoloadable<MarshallerAndUnmarshaller>> AUTOLOADABLE_MARSHALLERS = List.of(
-            autoloadIfClassPresent("de.quantummaid.mapmaid.jackson.JacksonJsonMarshaller"),
-            autoloadIfClassPresent("de.quantummaid.mapmaid.jackson.JacksonXmlMarshaller"),
-            autoloadIfClassPresent("de.quantummaid.mapmaid.jackson.JacksonYamlMarshaller")
+            autoloadIfClassPresent("de.quantummaid.mapmaid.jackson.JacksonJsonMarshaller")
     );
 
     private Map<MarshallingType, Marshaller> marshallerMap = smallMap();
